d02eb6afe87ed4905385b2276a8c06ae789d7b6f
maxrabin <max.rabin17@gmail.com>
2022-02-02 14:08:11 -0500

    mind grenade

--- node-js/3-modules.js
+++ node-js/3-modules.js
@@ -2,6 +2,9 @@
 const names = require('./4-names')
 const sayHi = require('./5-utils')
 const data = require('./6-alternative-flavor')
-console.log(data)
+require('./7-mind-grenade')
 
-sayHi(names.john)
+
+// sayHi('susan')
+// sayHi(names.peter)
+// sayHi(names.john)
