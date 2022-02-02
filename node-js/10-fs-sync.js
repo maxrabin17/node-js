d5fb490d9ce75807227357872b77d93e34f4b19f
maxrabin <max.rabin17@gmail.com>
2022-02-02 18:01:57 -0500

    reorganized

--- node-js/10-fs-sync.js
+++ node-js/10-fs-sync.js
@@ -1,5 +1,7 @@
 const {writeFileSync, readFileSync} = require('fs');
 
+
+console.log('start');
 const first = readFileSync('./content/first.txt', 'utf8')
 const second = readFileSync('./content/second.txt', 'utf8')
 
@@ -8,4 +10,5 @@ writeFileSync(
     `Here is the result: ${first}, ${second} `,
     {flag: 'a'}
 )
-console.log(first, second);
+console.log('done with this task');
+console.log('starting the next one');
