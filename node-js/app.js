ef1e94df45aa0d25ee31e6a661f150cd7706781f
maxrabin <max.rabin17@gmail.com>
2022-02-02 15:05:10 -0500

    built in modules

--- node-js/app.js
+++ node-js/app.js
@@ -5,4 +5,13 @@ const user = os.userInfo()
 // console.log(user)
 
 // method returns the system uptime in seconds
-console.log
+console.log(`The system uptime is ${os.uptime()} seconds`);
+
+const currentOS = {
+    name: os.type(),
+    release: os.release(),
+    totalMemory: os.totalmem(),
+    freeMem: os.freemem(),
+}
+
+console.log(currentOS)
